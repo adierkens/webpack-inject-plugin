fe8a5bc1a6a533392ffc8d9e77b2d3cd163b426b
Adam Dierkens <Adam_Dierkens@intuit.com>
2018-10-19 10:31:27 -0700

    Add example project

---
+++ example/webpack.config.js
@@ -0,0 +1,9 @@
+const InjectPlugin = require('..').default;
+
+module.exports = {
+    entry: './entry.js',
+    mode: 'development',
+    plugins: [
+        new InjectPlugin(() => `console.log('hello world');` )
+    ]
+};
